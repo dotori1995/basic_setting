6757e9d1e0782ca277202d23d7364e12357c3cc8
dotori1995 <dotori1995@gmail.com>
2020-07-21 00:24:05 +0900

    연습 시작용

---
+++ src/com/sbs/java/servlet/ArticleMainServlet.java
@@ -0,0 +1,25 @@
+package com.sbs.java.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/main")
+public class ArticleMainServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}

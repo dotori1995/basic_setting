e840e74f2a031e91bbabb64d9c6195011357f407
dotori1995 <dotori1995@gmail.com>
2020-07-21 16:01:05 +0900

    연습 시작용

--- src/com/sbs/java/servlet/ArticleMainServlet.java
+++ src/com/sbs/java/servlet/ArticleMainServlet.java
@@ -7,19 +7,62 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/article/main")
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+import com.sbs.java.util.DBUtil;
+import com.sbs.java.util.SecSql;
+
+@WebServlet("/article/list")
 public class ArticleMainServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 
+		String url = "jdbc:mysql://localhost:3306/(이곳에 DB DATABASE를 입력하시오)?serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeBehavior=convertToNull";
+		String user = "DB아이디를 입력하시오";
+		String password = "DB비밀번호를 입력하시오";
+
+		// 커넥터 드라이버 활성화
+		String driverName = "com.mysql.cj.jdbc.Driver";
+
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e) {
+			System.err.printf("[ClassNotFoundException 예외, %s]\n", e.getMessage());
+			response.getWriter().append("DB 드라이버 클래스 로딩 실패");
+			return;
+		}
+
+		// DB 연결
+		Connection con = null;
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+
+			
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
+	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
